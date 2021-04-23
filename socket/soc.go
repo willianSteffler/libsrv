f6b7d482c0f524e65f3aa24aa81f0541c0fe57d7
Willian Steffler <wstefflerdev@gmail.com>
2021-04-22 23:30:10 -0300

    soc package impprovements

--- socket/soc.go
+++ socket/soc.go
@@ -11,83 +11,87 @@ import (
 	"runtime/debug"
 )
 
+const  (
+	CRIAR_LIVRO = "criar"
+	REMOVER_LIVRO = "remover"
+	ALTERAR_LIVRO = "alterar"
+	BUSCAR_LIVRO = "buscar"
+)
+
 func handleConnection(c net.Conn, dao data.LibraryController) {
 	defer func() {
 		c.Close()
+		log.Printf("fechando conexão socket em %v\n",c.RemoteAddr().String())
 		if r := recover(); r != nil {
 			log.Printf("panic no tratamento da conexão !! %v , %s\n",r,string(debug.Stack()))
 		}
 	}()
-	var in, out []byte
-	var msg data.SocketMsg
-	var err error
-	if _, err = bufio.NewReader(c).Read(in); err != nil {
-		log.Printf("erro ao ler do socket %v\n ", err)
-		return
-	}
 
-	if err = proto.Unmarshal(in, &msg); err != nil {
-		log.Printf("mensagem não reconhecida %v\n", err)
-		return
-	}
-
-	resp := &data.SocketMsg{
-		Operacao: msg.GetOperacao() + ".resp",
-	}
-	switch msg.Operacao {
-	case "criarLivro":
-		if l := msg.GetLivro(); l != nil {
-			if err = dao.CriarLivro(l); err == nil {
-				resp.OpArgs = &data.SocketMsg_Livro{Livro: l}
-			}
-		} else {
-			err = fmt.Errorf("argumento livro deve ser fornecido")
+	for   {
+		var err error
+		var msg data.SocketMsg
+		if err,msg = GetSocketMessage(c); err != nil {
+			log.Println(err)
+			return
 		}
 
-	case "consultarLivro":
-		if c := msg.GetConsulta(); c != nil {
-			var livros []data.Livro
-			if err, livros = dao.ConsultarLivro(*c); err == nil {
-				var lr []*data.Livro
-				for _, livro := range livros {
-					lr = append(lr, &livro)
-				}
-				resp.OpArgs = &data.SocketMsg_ConsultaResp{
-					ConsultaResp: &data.ConsultarLivrosResp{Livros: lr},
-				}
-			}
-		} else {
-			err = fmt.Errorf("argumento livro deve ser fornecido")
-		}
-	case "removerLivro":
-		if l := msg.GetLivro(); l != nil {
-			if err = dao.RemoverLivro(l); err == nil {
-				resp.OpArgs = &data.SocketMsg_Livro{Livro: l}
-			}
-		} else {
-			err = fmt.Errorf("argumento livro deve ser fornecido")
-		}
-	case "alterarLivro":
-		if l := msg.GetLivro(); l != nil {
-			if err = dao.AlterarLivro(l); err == nil {
-				resp.OpArgs = &data.SocketMsg_Livro{Livro: l}
-			}
-		} else {
-			err = fmt.Errorf("argumento livro deve ser fornecido")
-		}
-	default:
-		err = fmt.Errorf("operação %s desconhecida", msg.Operacao)
-	}
-
-	if err != nil {
-		out, _ = proto.Marshal(&data.SocketMsg{
+		resp := data.SocketMsg{
 			Operacao: msg.GetOperacao() + ".resp",
-			OpArgs:   &data.SocketMsg_Erro{Erro: &data.Erro{Erro: err.Error()}},
-		})
-	}
+		}
+		switch msg.Operacao {
+		case CRIAR_LIVRO:
+			if l := msg.GetLivro(); l != nil {
+				if err = dao.CriarLivro(l); err == nil {
+					resp.OpArgs = &data.SocketMsg_Livro{Livro: l}
+				}
+			} else {
+				err = fmt.Errorf("argumento livro deve ser fornecido")
+			}
 
-	c.Write(out)
-	c.Close()
+		case BUSCAR_LIVRO:
+			if c := msg.GetConsulta(); c != nil {
+				var livros []data.Livro
+				if err, livros = dao.ConsultarLivro(*c); err == nil {
+					var lr []*data.Livro
+					for _, livro := range livros {
+						lr = append(lr, &livro)
+					}
+					resp.OpArgs = &data.SocketMsg_ConsultaResp{
+						ConsultaResp: &data.ConsultarLivrosResp{Livros: lr},
+					}
+				}
+			} else {
+				err = fmt.Errorf("argumento livro deve ser fornecido")
+			}
+		case REMOVER_LIVRO:
+			if l := msg.GetLivro(); l != nil {
+				if err = dao.RemoverLivro(l); err == nil {
+					resp.OpArgs = &data.SocketMsg_Livro{Livro: l}
+				}
+			} else {
+				err = fmt.Errorf("argumento livro deve ser fornecido")
+			}
+		case ALTERAR_LIVRO:
+			if l := msg.GetLivro(); l != nil {
+				if err = dao.AlterarLivro(l); err == nil {
+					resp.OpArgs = &data.SocketMsg_Livro{Livro: l}
+				}
+			} else {
+				err = fmt.Errorf("argumento livro deve ser fornecido")
+			}
+		default:
+			err = fmt.Errorf("operação %s desconhecida", msg.Operacao)
+		}
+
+		if err != nil {
+			resp.OpArgs =   &data.SocketMsg_Erro{Erro: &data.Erro{Erro: err.Error()}}
+		}
+
+		if err = SendSocketMessage(c,resp); err != nil {
+			log.Printf("erro ao escrever no socket %v\n",err)
+			return
+		}
+	}
 }
 
 func Listen(port int, db *gorm.DB) error {
@@ -109,4 +113,25 @@ func Listen(port int, db *gorm.DB) error {
 		}
 		go handleConnection(c, dao)
 	}
+}
+
+func GetSocketMessage(c net.Conn) (err error,msg data.SocketMsg){
+	var in []byte
+	if _, err = bufio.NewReader(c).Read(in); err != nil {
+		err = fmt.Errorf("erro ao ler do socket %v\n ", err)
+	} else if err = proto.Unmarshal(in, &msg); err != nil {
+		err = fmt.Errorf("mensagem não reconhecida %v\n", err)
+	}
+
+	return err,msg
+}
+
+func SendSocketMessage(c net.Conn,msg data.SocketMsg)(err error){
+	var out []byte
+	out, err = proto.Marshal(&msg)
+	if err != nil {
+		return err
+	}
+	_, err = c.Write(out)
+	return err
 }
